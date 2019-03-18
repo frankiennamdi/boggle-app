f568ac1a7cab8b5fed82667601ecd6f19fbae9af
frankiennamdi <frankie4real@yahoo.com>
2019-03-18 10:05:59 -0400

    use dictionary file

--- src/main/java/com/fanklin/sample/boggle/service/SampleBoggleService.java
+++ src/main/java/com/fanklin/sample/boggle/service/SampleBoggleService.java
@@ -4,8 +4,12 @@ import com.fanklin.sample.boggle.game.Boggle;
 import com.fanklin.sample.boggle.model.BoggleBoard;
 import com.fanklin.sample.boggle.model.BoggleResult;
 import com.fanklin.sample.boggle.support.SampleDictionary;
-import com.google.common.collect.ImmutableList;
+import com.google.common.io.Resources;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Component
 public class SampleBoggleService {
@@ -13,9 +17,13 @@ public class SampleBoggleService {
   private final Boggle boggle;
 
   public SampleBoggleService() {
-    SampleDictionary sampleDictionary = new SampleDictionary(ImmutableList.of(
-            "GEEKS", "FOR", "QUIZ", "GO"
-    ));
+    List<String> dictionaryWords;
+    try {
+      dictionaryWords = Resources.readLines(Resources.getResource("sample_dictionary.txt"), StandardCharsets.UTF_8);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    SampleDictionary sampleDictionary = new SampleDictionary(dictionaryWords);
     boggle = new Boggle(sampleDictionary);
   }
 
